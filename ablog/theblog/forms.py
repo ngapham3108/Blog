6005e9ef22e93d11143fb2ec315c0c13afb63725
phnga <ngapham3108@gmail.com>
2020-11-26 23:06:23 +0700

    Add Category

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -1,23 +1,29 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
 
+list_category = Category.objects.all()
+choices = []
+for cat in list_category:
+    choices.append((cat.name, cat.name))
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'cats', 'body')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title of your blog'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'cats': forms.Select(choices=choices),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'body')
+        fields = ('title', 'title_tag', 'cats', 'body')
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title of your blog'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'cats': forms.Select(choices=choices),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -4,10 +4,13 @@ from django.urls import reverse
 from datetime import datetime, date
 # Create your models here.
 
+
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=20, default='Blog Site')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    cats = models.CharField(max_length=50, default='Others')
     body = models.TextField()
     post_date = models.DateTimeField(auto_now_add=True)
 
@@ -16,3 +19,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('article-detail', args=[str(self.pk)])
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
